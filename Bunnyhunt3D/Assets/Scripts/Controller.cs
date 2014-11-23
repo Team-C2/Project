58c0ea6b78220e9e0485cb5f3d781f15190db385
Tom Schrauwen <tom-tom.s@hotmail.com>
2014-11-23 15:44:48 +0000

    controller script

---
+++ Bunnyhunt3D/Assets/Scripts/Controller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+    public float movementSpeed = 50;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal * movementSpeed, 0.0f ,moveVertical * movementSpeed);
+        rigidbody.velocity = movement;
+
+    }
+}
