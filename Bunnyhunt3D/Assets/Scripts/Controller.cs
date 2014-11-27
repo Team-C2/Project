94dd012acc83c6360634ddd0e6ccdaf9bf39729e
Tom Schrauwen <tom-tom.s@hotmail.com>
2014-11-27 12:12:03 +0000

    Controller fixed
    gefixed controller script.

--- Bunnyhunt3D/Assets/Scripts/Controller.cs
+++ Bunnyhunt3D/Assets/Scripts/Controller.cs
@@ -14,14 +14,24 @@ public class Controller : MonoBehaviour {
 	
 	}
 
-    void FixedUpdate()
-    {
-
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveHorizontal * movementSpeed, 0.0f ,moveVertical * movementSpeed);
-        rigidbody.velocity = movement;
-
-    }
+	void FixedUpdate()
+	{
+		if (this.gameObject.tag == "bunny" && (Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right")))
+		{
+			float moveHorizontal = Input.GetAxis("Horizontal");
+			float moveVertical = Input.GetAxis("Vertical");
+			
+			Vector3 movement = new Vector3(moveHorizontal * movementSpeed, 0f, moveVertical * movementSpeed);
+			this.rigidbody.velocity = movement;
+		}
+		
+		if (this.gameObject.tag == "hunter" && (Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d") || Input.GetKey("w")))
+		{
+			float moveHorizontal = Input.GetAxis("Horizontal");
+			float moveVertical = Input.GetAxis("Vertical");
+			
+			Vector3 movement = new Vector3(moveHorizontal * movementSpeed, 0f, moveVertical * movementSpeed);
+			this.rigidbody.velocity = movement;
+		}
+	}
 }
