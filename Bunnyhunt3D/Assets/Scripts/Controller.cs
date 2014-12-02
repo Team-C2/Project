ad4059116da950dd97c9584ac3b39e75cc6dbf37
Tom Schrauwen <tom-tom.s@hotmail.com>
2014-12-02 15:59:32 +0000

    Timer script toegevoegd
    timer script toegevoegd aan de main camera. Hij doet nog niets. Ik heb
    Application.quit(); gezet als hij op 0 komt, maar daar gebeurt nog niets
    door.

--- Bunnyhunt3D/Assets/Scripts/Controller.cs
+++ Bunnyhunt3D/Assets/Scripts/Controller.cs
@@ -14,26 +14,6 @@ public class Controller : MonoBehaviour {
 	
 	}
 
-	/*void FixedUpdate()
-	{
-		if (this.gameObject.tag == "bunny" && (Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right")))
-		{
-			float moveHorizontal = Input.GetAxis("Horizontal");
-			float moveVertical = Input.GetAxis("Vertical");
-			
-			Vector3 movement = new Vector3(moveHorizontal * movementSpeed, 0f, moveVertical * movementSpeed);
-            GameObject.FindGameObjectWithTag("bunny").rigidbody.velocity = movement;
-		}
-		
-		if (this.gameObject.tag == "hunter" && (Input.GetKey("w") || Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d")))
-		{
-			float moveHorizontal = Input.GetAxis("Horizontal2");
-			float moveVertical = Input.GetAxis("Vertical2");
-			
-			Vector3 movement = new Vector3(moveHorizontal * movementSpeed, 0f, moveVertical * movementSpeed);
-			GameObject.FindGameObjectWithTag("hunter").rigidbody.velocity = movement;
-		}
-	}*/
 	void FixedUpdate()
 	{
 		if (this.gameObject.tag == "bunny")
---
+++ Bunnyhunt3D/Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown : Global
+{
+	public float timeLeft = 100.0f;
+	
+	public void Update()
+	{
+		timeLeft -= Time.deltaTime;
+		
+		if (timeLeft <= 0.0f)
+		{
+			Application.Quit ();
+			guiText.text = "You ran out of time";
+		}
+		else
+		{
+			guiText.text = "Time left = " + (int)timeLeft + " seconds";
+		}
+		
+	}
+	
+}
