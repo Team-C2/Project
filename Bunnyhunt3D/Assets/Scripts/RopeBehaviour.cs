7a20b8967acdf9411d4da65d558227afafd36e37
Tom Schrauwen <tom-tom.s@hotmail.com>
2014-12-04 14:06:50 +0000

    ropeBehaviour update
    de rotatie werkt nu half

--- Bunnyhunt3D/Assets/Scripts/RopeBehaviour.cs
+++ Bunnyhunt3D/Assets/Scripts/RopeBehaviour.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class RopeBehaviour : Global {
 
 	public bool activeRope;
-	private Collider hunter;
+	public Transform hunter;
 	public Transform ropePrefab;
 	
 	void Start () 
@@ -19,15 +19,19 @@ public class RopeBehaviour : Global {
 	{
 		if (activeRope == true)
 		{
-			//ScaleToHunter (this, hunter);
+			ScaleToHunter (hunter);
 		}
 
 	}
 
-	private void ScaleToHunter(GameObject objectToScale, Collider hunter) 
+	private void ScaleToHunter(Transform hunter) 
 	{
-
-		
+		Vector3 dir = hunter.transform.position - this.transform.position;
+		float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+		angle -= 90;
+		angle = Mathf.Round(angle/6.0f)*6.0f;
+		Quaternion q = Quaternion.AngleAxis(angle, Vector3.down);
+		this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, q, 6);
 	}
 
 	void OnCollision(Collider other)
