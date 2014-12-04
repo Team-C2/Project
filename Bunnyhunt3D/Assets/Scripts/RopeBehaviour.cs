652b33021e5f54bf3e1ab8b7dccefff3495a64d9
Tom Schrauwen <tom-tom.s@hotmail.com>
2014-12-04 13:24:21 +0000

    Rope (niet af!)
    geen errors maar het werkt nog niet

--- Bunnyhunt3D/Assets/Scripts/RopeBehaviour.cs
+++ Bunnyhunt3D/Assets/Scripts/RopeBehaviour.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RopeBehaviour : MonoBehaviour {
+public class RopeBehaviour : Global {
 
-	// Use this for initialization
-	void Start () {
+	public bool activeRope;
+	private Collider hunter;
+	public Transform ropePrefab;
+	
+	void Start () 
+	{
+		activeRope = true;
+		//hunter = GameObject.FindGameObjectsWithTag ("hunter");
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if (activeRope == true)
+		{
+			//ScaleToHunter (this, hunter);
+		}
+
+	}
+
+	private void ScaleToHunter(GameObject objectToScale, Collider hunter) 
+	{
+
+		
+	}
+
+	void OnCollision(Collider other)
+	{
+		if (other.tag == "pole") 
+		{
+			this.activeRope = false;
+			Instantiate(ropePrefab, other.transform.position, Quaternion.identity);
+		}
+
 	}
 }
